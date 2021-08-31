665e6014e6510e89edef98453402c929ed44e7f7
yeonnex <yeonnex@gmail.com>
2021-08-31 22:15:35 +0900

    CreateView를 통해 장고 기본 제공 로그인 기능 구현

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,11 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
-
 
 def hello_world(request):
     if request.method == 'POST':
@@ -22,3 +24,9 @@ def hello_world(request):
     else:  # GET 등 post 를 제외한 나머지 메서드
         hello_world_list = HelloWorld.objects.all()
         return render(request,'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+class AccountCreateView(CreateView): # CreateView를 상속받는다.
+    model = User # 장고에서 기본으로 제공헤주는 User라는 모델 사용!
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
