a71aecf3160487e1c736b9c4479bdfd73c3945ef
yeonnex <yeonnex@gmail.com>
2021-09-04 13:56:32 +0900

    UpdateView를 이용한 비밀번호 변경 구현. accountapp에서 forms.py 만들어서 수정폼 유저네임 비활성화하게 커스터마이징하였음

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,8 +5,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, FormView, DetailView
+from django.views.generic import CreateView, FormView, DetailView, UpdateView
 
+from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
 def hello_world(request):
@@ -36,4 +37,10 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'
 
