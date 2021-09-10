bc30bdfd054ee3c3d3d384bf6d6e94721ae8afd7
yeonnex <yeonnex@gmail.com>
2021-09-10 22:15:12 +0900

    31강 Profileapp의 CreateView 집중구현, 이미지 파일을 form으로 전송하기 위해서는 enctype="multipart/form-data" 를 폼 태그에 명시해주어야 함

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/create.html'
+
+    def form_valid(self, form):  # 이미지와 메시지, 닉네임을 담은 날라온 폼 데이터가 form 에들어있음
+        temp_profile = form.save(commit=False)  # 그 데이터를 임시로 저장. 실제 db에 저장하지 않고!
+        # 우리가 보낸 세가지의 데이터는 있는데, user라는 데이터가 아직 없는 것. 그래서 만들어주자.
+        temp_profile.user = self.request.user  # 요청을 보낸 당사자 유저로 유저 저장
+        temp_profile.save()  # 그제서야 최종 저장!!!
+        return super().form_valid(form)
